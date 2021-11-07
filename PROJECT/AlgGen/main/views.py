0e372a7590212303eb4f5c1cf7b4fd85a35738a3
EgorFokin <egor.fokin2017@gmail.com>
2021-11-07 20:04:15 +0300

    final code

--- PROJECT/AlgGen/main/views.py
+++ PROJECT/AlgGen/main/views.py
@@ -216,7 +216,6 @@ def activate_account(request, uidb64, token):
         user = User.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
-    print(token)
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
